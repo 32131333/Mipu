277ac5c74498ab96ee27d0bd45033cb231c18115
d32131333 <7560yhh@gmail.com>
2026-04-25 22:05:26 +0700

    commit

--- page/translatable_static/templates/c.jsx
+++ page/translatable_static/templates/c.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 const { useEffect, useState } = React;
 import { useImmer } from "use-immer";
-import { Link } from "react-router";
+import { Link, NavLink } from "react-router";
 
 
 const st = <style>{`
@@ -21,7 +21,7 @@ const st = <style>{`
 
 
 function ChatCard({children}) {
-	return <Link to="." className={["app-centercard", children.unread > 0 && "unread"].filter(Boolean).join(" ")}>
+	return <NavLink to={String(children.id)} className={["app-centercard", children.unread > 0 && "unread"].filter(Boolean).join(" ")}>
 		<app.components.Avatar userId={children.id} userAvatar={{media: children.icon}}/>
 		<div>
 			<div id="n"><span>{children.name}</span></div>
@@ -31,46 +31,47 @@ function ChatCard({children}) {
 			}</span>
 		</div>
 		{children.unread > 0 && <div></div>}
-	</Link>;
+	</NavLink>;
 };
 
+export const path = "c/:page?";
 export default function Center() {
 	const { me } = app.reactstates.useInformationAboutMe();
 	
 	// ПРОТОТИП
 	const [ chats, updateChats ] = useImmer([
 		{
-			id: 2, icon: "/something1",
+			id: 8, icon: "something5",
 			name: "test",
 			unread: 0, lastMessage: "Oh, hi?",
 			lastMessageDate: 1777123734344
 		},
 		{
-			id: 3, icon: "/something1",
+			id: 7, icon: "something4",
 			name: "Chat",
 			unread: 0, lastMessage: "What with you?",
 			lastMessageDate: 1777123734345
 		},
 		{
-			id: 4, icon: "/something1",
+			id: 6, icon: "something3",
 			name: "12312312312",
 			unread: 0, lastMessage: "You feel bad?",
 			lastMessageDate: 1777123734344
 		},
 		{
-			id: 4, icon: "/something1",
+			id: 5, icon: "something2",
 			name: "...",
 			unread: 0, lastMessage: null,
 			lastMessageDate: null
 		},
 		{
-			id: 4, icon: "/something1",
+			id: 4, icon: "something1",
 			name: "TEST",
 			unread: 0, lastMessage: "Don't worry",
 			lastMessageDate: null
 		},
 		{
-			id: 1, icon: "/something",
+			id: 1, icon: "something",
 			name: "mipu✨",
 			unread: 1, lastMessage: "i love you",
 			lastMessageDate: Date.now()
